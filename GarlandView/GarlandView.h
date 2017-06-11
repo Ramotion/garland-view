09e67f6bd4746010d4e1f42c7fa8e508d9da740f
Yusupov <boxxer25@gmail.com>
2017-06-11 17:37:14 +0300

    Initial Commit

---
+++ GarlandView/GarlandView.h
@@ -0,0 +1,19 @@
+//
+//  GarlandView.h
+//  GarlandView
+//
+//  Created by Slava Юсупов on 11.06.17.
+//  Copyright © 2017 Slava Юсупов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GarlandView.
+FOUNDATION_EXPORT double GarlandViewVersionNumber;
+
+//! Project version string for GarlandView.
+FOUNDATION_EXPORT const unsigned char GarlandViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GarlandView/PublicHeader.h>
+
+
