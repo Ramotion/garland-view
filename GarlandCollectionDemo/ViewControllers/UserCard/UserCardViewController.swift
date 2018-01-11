9773d567ce76517e7e6c382ce01c6955408e7091
i.kolpachkov <i.kolpachkov@gmail.com>
2018-01-11 15:53:15 +0300

    remove user profile details transition

--- GarlandCollectionDemo/ViewControllers/UserCard/UserCardViewController.swift
+++ GarlandCollectionDemo/ViewControllers/UserCard/UserCardViewController.swift
@@ -15,51 +15,15 @@ class UserCardViewController: GarlandCardController {
     @IBOutlet var closeButton: UIButton!
     @IBOutlet var detailsButton: UIButton!
     
-    fileprivate var isDetailed = false
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         closeButton.addTarget(self, action: #selector(closeButtonAction), for: .touchDown)
-        detailsButton.addTarget(self, action: #selector(detailsButtonAction), for: .touchDown)
         
         let nib = UINib(nibName: "CardCollectionCell", bundle: nil)
         garlandCardCollection.register(nib, forCellWithReuseIdentifier: "CardCollectionCell")
     }
     
     //MARK: Actions
-    @objc fileprivate func detailsButtonAction() {
-        let cardConstraits = card.findSuperviewConstraints(attribute: .leading)
-        
-        let centerYCardConstrait = card.findSuperviewConstraints(attribute: .centerY)
-        
-        isDetailed = true
-        for cell in garlandCardCollection.visibleCells {
-            cell.layer.cornerRadius = 5.0
-        }
-        
-        UIView.animateKeyframes(withDuration: 0.2, delay: 0, options: .calculationModeLinear, animations: {
-            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0, animations: {
-                self.card.layer.cornerRadius = 0
-                cardConstraits[0].constant = 0
-                centerYCardConstrait[0].constant = 100
-            })
-        }, completion: {_ in
-            let cellSize = CGSize(width: 300 , height: 110)
-            
-            let sideInset = (self.card.frame.width - cellSize.width)/2
-            
-            let layout = UICollectionViewFlowLayout()
-            layout.scrollDirection = .vertical
-            layout.itemSize = cellSize
-            layout.sectionInset = UIEdgeInsets(top: 5, left: sideInset, bottom: 5, right: sideInset)
-            layout.minimumLineSpacing = 5.0
-            layout.minimumInteritemSpacing = 5.0
-            
-            self.garlandCardCollection.setCollectionViewLayout(layout, animated: true)
-            self.garlandCardCollection.reloadData()
-        })
-    }
-    
     @objc fileprivate func closeButtonAction() {
         dismiss(animated: true, completion: nil)
     }
@@ -81,9 +45,6 @@ extension UserCardViewController: UICollectionViewDataSource, UICollectionViewDe
             return UICollectionViewCell()
         }
         
-        if isDetailed {
-            cell.layer.cornerRadius = 5.0
-        }
         cell.contentMode = .scaleToFill
         return cell
     }
